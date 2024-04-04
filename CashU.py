072e25471c8e5ef1de7a84bdb2542901bca00d95
edwardb <baxtere@macstudent.com.au>
2024-04-05 09:48:01 +1100

    finished writing the Money_buckets function

--- CashU.py
+++ CashU.py
@@ -17,3 +17,24 @@ while choice != "":
     choice = int(input("Enter [1] for money buckets, \nEnter [2] for compound interest: "))
 
 
+def Money_buckets():
+    print("Welcome to money buckets!")
+    amount = input("Enter an amount to begin: ")
+
+    blow = amount * 0.6
+    grow = amount * 0.2
+    mojo = amount * 0.2
+
+    daily = blow * 0.6
+    splurge = blow * 0.1
+    smile = blow * 0.1
+    extinguisher = blow * 0.2
+
+    print("Your blow (60%) is: $" + blow)
+    print("Making up your blow is: $")
+    print("Daily (60%): $" + daily)
+    print("Splurge (10%): $" + splurge)
+    print("Smile (10%): $" + smile)
+    print("Fire extinguisher (20%): $" + extinguisher)
+    print("Your grow (20%) is: $" + grow)
+    print("Your mojo (20%) is: $" + mojo)
