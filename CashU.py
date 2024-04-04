b65747cf07a90960e019196a8a2ba1fe06753ad6
edwardb <baxtere@macstudent.com.au>
2024-04-05 09:41:28 +1100

    Completed main program of CashU, excluding the Money_buckets and Compound_interest functions

--- CashU.py
+++ CashU.py
@@ -1,3 +1,19 @@
 print("Welcome to 'CashU'!")
 
 choice = int(input("Enter [1] for money buckets, \nEnter [2] for compound interest: "))
+
+while choice != "":
+    if choice == 1:
+        Money_buckets()
+
+    elif choice == 2:
+        Compound_interest()
+
+    else:
+        print("Not a valid input!")
+
+    print("----------------------------")
+    print("Enter nothing to exit.")
+    choice = int(input("Enter [1] for money buckets, \nEnter [2] for compound interest: "))
+
+
